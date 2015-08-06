2d09592bdb0dc360f3e13497fb08ab03c46a980e
Andrey Lushnikov <lushnikov@chromium.org>
2015-08-06 12:38:43 -0700

    small changes

--- Shim.js
+++ Shim.js
@@ -6,17 +6,19 @@ require("./ParsedURL.js");
 require("./SourceMap.js");
 
 var fs = require("fs");
+var mapURL = "res/all.css.map";
 
-fs.readFile("all.css.map", "utf-8", function(error, text) {
+fs.readFile(mapURL, "utf-8", function(error, text) {
     if (error || !text) {
         console.error("Failed to load sourceMap.");
         return;
     }
     var payload = JSON.parse(text);
-    var sourceMap = new WebInspector.SourceMap("all.css.map", payload);
+    var sourceMap = new WebInspector.SourceMap(mapURL, payload);
     console.log(sourceMap.findEntry(0, 0));
     console.log(sourceMap.findEntry(0, 1));
     console.log(sourceMap.findEntry(0, 2));
     console.log(sourceMap.findEntry(0, 3));
     console.log(sourceMap.findEntry(0, 4));
+    console.log(sourceMap.sources());
 });
